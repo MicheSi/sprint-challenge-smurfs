70af2c1c6a7e4214af14ffb95cdb2a575bbf9967
mlazzell17@gmail.com <MicheSi>
2020-01-24 10:26:56 -0700

    created form to add smurf

---
+++ smurfs/src/components/AddSmurfForm.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+
+const AddSmurfForm = props => {
+    const initialState = {
+        name: '',
+        age: '',
+        height: '',
+        id: Date.now()
+    }
+
+    const [newSmurf, setNewSmurf] = useState(initialState)
+
+    const handleChanges = e => {
+        setNewSmurf({
+            ...newSmurf,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setNewSmurf(initialState)
+    }
+
+    return (
+        <div>
+            <h2>Add a new Smurf</h2>
+            <form onSubmit={handleSubmit}>
+                    <input
+                    type='text'
+                    name='name'
+                    placeholder='Name'
+                    value={newSmurf.name}
+                    onChange={handleChanges}
+                    />
+                    <input
+                    type='text'
+                    name='age'
+                    placeholder='Age'
+                    value={newSmurf.age}
+                    onChange={handleChanges}
+                    />
+                    <input
+                    type='text'
+                    name='height'
+                    placeholder='Height'
+                    value={newSmurf.height}
+                    onChange={handleChanges}
+                    />
+                    <button type='submit'>Moving Smurf to Village...</button>
+            </form>
+        </div>
+        
+    )
+}
+
+export default AddSmurfForm;
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 
 import Smurf from './smurfs';
+import AddSmurfForm from './AddSmurfForm';
 
 import "./App.css";
+
 
 
 class App extends Component {
@@ -11,6 +13,7 @@ class App extends Component {
       <div className="App">
         <h1>My Smurf Village</h1>
         <Smurf />
+        <AddSmurfForm />
       </div>
     );
   }
