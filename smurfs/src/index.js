34f405df5b46e7cfe7f3e09b0eb181f349fd396e
mlazzell17@gmail.com <MicheSi>
2020-01-24 09:13:11 -0700

    added store to index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.css";
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>,
+document.getElementById("root"));
